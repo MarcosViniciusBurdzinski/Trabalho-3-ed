60fa01a50e38a95277454b9895683c9bea690a52
MarcosViniciusBurdzinski <marcosviniciusburdzinskim@gmail.com>
2026-05-13 08:22:00 -0300

    Estruturas criadas

---
+++ Matchmaking.cpp
@@ -0,0 +1,11 @@
+#include "Matchmaking.hpp"
+
+Matchmaking::Matchmaking() {};
+Matchmaking::~Matchmaking() {};
+bool Matchmaking::insert(Player player) {};
+bool Matchmaking::removePlayer(int id) {};
+void Matchmaking::sortByScoreInsertion() {};
+void Matchmaking::sortByScoreMerge() {};
+Player* Matchmaking::formGroup(int groupSize, int delta, int* n) {};
+Player* Matchmaking::getWaitingPlayers(int* n) {};
+void Matchmaking::printWaitingPlayers() {};
